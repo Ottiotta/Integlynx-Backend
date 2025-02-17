dd2bd3db93af9e2206db8bb63e1201fe8f87e2af
Beolinen <26201164-Beolinen@users.noreply.replit.com>
2025-02-17 15:43:18 +0000

    User checkpoint: Improve logging: Enhance logging functionality by adding template support and detailed source information.

--- logging.js
+++ logging.js
@@ -1,20 +1,38 @@
 const fs = require("node:fs");
 const config = JSON.parse(fs.readFileSync("logging.json"));
+String.prototype.applyTemplates = applyTemplates;
 
 function getCallerInfo() {
   const err = new Error();
   const stack = err.stack.split("\n")[3]; // Get caller of logging function
+  log("INFO", { source: "getCallerInfo" }, stack);
   const caller =
     stack.match(/at\s+(.*)\s+\((.*):(\d+):(\d+)\)/) ||
     stack.match(/at\s+()(.*):(\d+):(\d+)/);
+  log("INFO", { source: "getCallerInfo" }, caller);
   if (caller) {
-    return { 
-      fileName: caller[2].split("/")[-1], 
-      filePath: caller[2].replace(/.*\//, ""), 
-      lineNumber: caller[3] 
+    return {
+      file: caller[2].split("/")[-1],
+      path: caller[2].replace(/.*\//, ""),
+      line: caller[3],
+      function: "fun" /*add function name*/,
     };
   }
   return "unknown";
+}
+
+function applyTemplates(
+  templates,
+  templateStart = "${",
+  templateEnd = "}",
+) {
+  templates.forEach((template) => {
+    text = text.replace(
+      templateStart + template.key + templateEnd,
+      template.value,
+    );
+  });
+  return text;
 }
 
 function debug(text) {
@@ -33,13 +51,16 @@ function error(text) {
   log("ERROR", getCallerInfo(), text);
 }
 
-function log(level, source, text) {
+function log(level, sourceobj, text) {
   const template = config.templates[level] || config.templates.default;
-  
+  let source = config.templates.source.applyTemplates(sourceobj);
   const output = template
-    .replace("${level}", level)
-    .replace("${source}", source)
-    .replace("${text}", text);
+    .applyTemplates({
+      level: level,
+      source: source,
+      text: text,
+    })
+    .applyTemplates(sourceobj);
   console.log(output);
 }
 
